2b29ab5a875c714d06e0eb91c4c51050a18813ad
Mustafa KURT <trukafatsum@gmail.com>
2024-11-12 06:22:55 +0300

    Döngüler ve iç içe döngüler
    +Yıldız karakterini kullanarak konsol ekranına çeşitli şekiller yazdırdık

---
+++ 05_LoopsWithStars/Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_LoopsWithStars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Alt alta 10 tane yıldız oluşturma
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine('*');
+            //}
+            #endregion
+
+            #region Yan yana 10 tane yıldız oluşturma
+            //for (int i = 0; i <= 10; i++)
+            //{
+            //    Console.Write('*');
+            //}
+            #endregion
+
+            #region Alt alta 10 tane yıldız oluşturma her satırda 10 tane yıldız olsun
+            //for (int i = 0; i <= 10; i++)
+            //{
+            //    Console.WriteLine("**********");
+            //}
+            #endregion
+
+            #region Diküçgen oluşturma
+            /*
+             Örnek:
+            *
+            **
+            ***
+            ****
+            *****
+            ******
+             */
+
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    for(int j = 1; j <= i; j++)
+            //        Console.Write("*");
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region Ters Diküçgen
+            //for (int i = 5; i >= 1; i--)
+            //{
+            //    for(int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write('*');
+            //    }
+            //    Console.WriteLine();
+            //}
+            #endregion
+
+            #region Yan Eşkenar üçgen
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write('*');
+            //    }
+            //    Console.WriteLine();
+            //}
+            //for (int k = 4; k >= 1; k--)
+            //{
+            //    for (int m = 1; m <= k; m++)
+            //    {
+            //        Console.Write('*');
+            //    }
+            //    Console.WriteLine();
+            //}
+            #endregion
+
+            #region Baklava Dilimi
+            /*
+                 * 
+                ***
+               *****
+              *******
+               *****
+                ***
+                 * 
+             */
+
+            //int n = 5;
+
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    for (int j = i; j < n; j++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for (int k = 1; k <= (2 * i - 1); k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //for (int i = n - 1; i >= 1; i--)
+            //{
+            //    for (int j = n; j > i; j--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+
+            //    for (int k = 1; k <= (2 * i - 1); k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+
+            //    Console.WriteLine();
+            //}
+            #endregion
+
+            #region Piramit
+
+            //int n = 5;
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    for(int j = n - i; j > 0; j--)
+            //    {
+            //        Console.Write(' ');
+            //    }
+            //    for(int k = 1; k <= 2 * i - 1; k++)
+            //    {
+            //        Console.Write('*');
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region Ters Piramit
+
+            //int n = 5;
+
+            //for (int i = n; i >= 1; i--)
+            //{
+            //    for(int j = n - i; j > 0 ; j--)
+            //    {
+            //        Console.Write(' ');
+            //    }
+            //    for (int k = 1; k <= 2 * i - 1; k++)
+            //        Console.Write('*');
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+
+            Console.Read();
+        }
+    }
+}
