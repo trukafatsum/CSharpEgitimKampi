db1ebc001f31d537734fa1d34b6e4287227e0b48
Mustafa KURT <trukafatsum@gmail.com>
2024-11-19 08:25:57 +0300

    Add project files.

---
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.EntityLayer.Concrete
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public bool CategoryStatus { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
+
+/*
+ Field-Variable-Property
+
+**Code First yaklaşımında otomatik artan ve birincil anahtar olarak algılaması için sınıfın adı yazılmalı ve sonuna Id getirilmelidir.
+**Category sınıfı için CategoryId doğru tanımdır. Category sınıfı için ProductId veya AbcId yazılırsa bunu birincil anahtar olarak oluşturmaz.**
+
+
+Bire çok ilişki için List<Product> Products tanımlandı.
+ */
---
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.EntityLayer.Concrete
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public int ProductStock { get; set; }
+        public decimal ProductPrice { get; set; }
+        public string ProductDescription { get; set; }
+
+        //CategoryID ile bire çok ilişkiyi kurmak için virtal Category sınıfından bir prop. tanımlandı.
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+
+        public List<Order> Orders { get; set; }
+    }
+}
